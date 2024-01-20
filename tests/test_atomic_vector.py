dee8261782554e50e713c328c51b8614031e0b7b
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-01-19 21:58:26 -0800

    Test for correct loading of atomic vectors.

--- tests/test_atomic_vector.py
+++ tests/test_atomic_vector.py
@@ -2,7 +2,9 @@ import dolomite_base as dl
 from biocutils import StringList, IntegerList, FloatList, BooleanList
 from tempfile import mkdtemp
 import h5py
+import numpy
 import os
+import pytest
 
 def test_string_list_basic():
     sl = StringList([1,2,3,4])
@@ -46,6 +48,24 @@ def test_integer_list_basic():
     assert roundtrip == sl
 
 
+def test_integer_list_ndarray():
+    sl = IntegerList([1,2,3,4])
+    dir = os.path.join(mkdtemp(), "temp")
+    dl.save_object(sl, dir)
+    roundtrip2 = dl.read_object(dir, atomic_vector_use_numeric_1darray=True)
+    assert isinstance(roundtrip2, numpy.ndarray)
+    assert roundtrip2.dtype == numpy.int32
+    assert list(roundtrip2) == sl.as_list()
+
+    sl = IntegerList([1,2,None,3,4])
+    dir = os.path.join(mkdtemp(), "temp")
+    dl.save_object(sl, dir)
+    roundtrip2 = dl.read_object(dir, atomic_vector_use_numeric_1darray=True)
+    assert isinstance(roundtrip2, numpy.ma.MaskedArray)
+    assert roundtrip2.dtype == numpy.int32
+    assert [None if numpy.ma.is_masked(y) else int(y) for y in roundtrip2] == sl.as_list()
+
+
 def test_integer_list_large():
     sl = IntegerList([2**32,4**20])
     dir = os.path.join(mkdtemp(), "temp")
@@ -64,6 +84,9 @@ def test_integer_list_names():
     assert isinstance(roundtrip, IntegerList)
     assert roundtrip == sl
 
+    with pytest.warns(UserWarning, match="skipping names"):
+        roundtrip = dl.read_object(dir, atomic_vector_use_numeric_1darray=True)
+
 
 def test_float_list_basic():
     sl = FloatList([1,2,3,4])
@@ -79,6 +102,24 @@ def test_float_list_basic():
     roundtrip = dl.read_object(dir)
     assert isinstance(roundtrip, FloatList)
     assert roundtrip == sl
+
+
+def test_float_list_ndarray():
+    sl = FloatList([1.2,2.3,3.4,4.5])
+    dir = os.path.join(mkdtemp(), "temp")
+    dl.save_object(sl, dir)
+    roundtrip2 = dl.read_object(dir, atomic_vector_use_numeric_1darray=True)
+    assert isinstance(roundtrip2, numpy.ndarray)
+    assert roundtrip2.dtype == numpy.float64
+    assert list(roundtrip2) == sl.as_list()
+
+    sl = FloatList([1.2,None,2.3,None,3.4,4.5])
+    dir = os.path.join(mkdtemp(), "temp")
+    dl.save_object(sl, dir)
+    roundtrip2 = dl.read_object(dir, atomic_vector_use_numeric_1darray=True)
+    assert isinstance(roundtrip2, numpy.ma.MaskedArray)
+    assert roundtrip2.dtype == numpy.float64
+    assert [None if numpy.ma.is_masked(y) else float(y) for y in roundtrip2] == sl.as_list()
 
 
 def test_float_list_names():
@@ -105,6 +146,24 @@ def test_boolean_list_basic():
     roundtrip = dl.read_object(dir)
     assert isinstance(roundtrip, BooleanList)
     assert roundtrip == sl
+
+
+def test_boolean_list_ndarray():
+    sl = BooleanList([True, False, False, True])
+    dir = os.path.join(mkdtemp(), "temp")
+    dl.save_object(sl, dir)
+    roundtrip2 = dl.read_object(dir, atomic_vector_use_numeric_1darray=True)
+    assert isinstance(roundtrip2, numpy.ndarray)
+    assert roundtrip2.dtype == numpy.bool_
+    assert list(roundtrip2) == sl.as_list()
+
+    sl = BooleanList([True, False, None, False, True])
+    dir = os.path.join(mkdtemp(), "temp")
+    dl.save_object(sl, dir)
+    roundtrip2 = dl.read_object(dir, atomic_vector_use_numeric_1darray=True)
+    assert isinstance(roundtrip2, numpy.ma.MaskedArray)
+    assert roundtrip2.dtype == numpy.bool_
+    assert [None if numpy.ma.is_masked(y) else bool(y) for y in roundtrip2] == sl.as_list()
 
 
 def test_boolean_list_names():
