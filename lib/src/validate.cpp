7ca507d4158122d3bee35429144d7a42bfdcc53d
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-02-01 14:29:22 -0800

    Provide an opportunity for custom validators.

--- lib/src/validate.cpp
+++ lib/src/validate.cpp
@@ -1,5 +1,123 @@
 #include "takane/takane.hpp"
+#include "pybind11/pybind11.h"
 
-void validate(std::string path) {
-    takane::validate(path);
+static takane::Options global_options;
+
+std::shared_ptr<millijson::Base> convert_to_millijson(const pybind11::handle& x) {
+    std::shared_ptr<millijson::Base> output;
+
+    if (pybind11::isinstance<pybind11::none>(x)) {
+        output.reset(new millijson::Nothing);
+    } else if (pybind11::isinstance<pybind11::bool_>(x)) {
+        output.reset(new millijson::Boolean(pybind11::cast<bool>(x)));
+    } else if (pybind11::isinstance<pybind11::int_>(x)) {
+        output.reset(new millijson::Number(pybind11::cast<double>(x)));
+    } else if (pybind11::isinstance<pybind11::float_>(x)) {
+        output.reset(new millijson::Number(pybind11::cast<double>(x)));
+    } else if (pybind11::isinstance<pybind11::str>(x)) {
+        output.reset(new millijson::String(pybind11::cast<std::string>(x)));
+    } else if (pybind11::isinstance<pybind11::list>(x)) {
+        auto y = pybind11::reinterpret_borrow<pybind11::list>(x);
+        auto aptr = new millijson::Array;
+        output.reset(aptr);
+        for (size_t e = 0, end = y.size(); e < end; ++e) {
+            aptr->add(convert_to_millijson(y[e]));
+        }
+    } else if (pybind11::isinstance<pybind11::dict>(x)) {
+        auto y = pybind11::reinterpret_borrow<pybind11::dict>(x);
+        auto optr = new millijson::Object;
+        output.reset(optr);
+        for (auto it = y.begin(); it != y.end(); ++it) {
+            auto field = pybind11::cast<std::string>(it->first);
+            optr->add(std::move(field), convert_to_millijson(it->second));
+        } 
+    } else {
+        throw std::runtime_error("cannot convert unknown python object to JSON");
+    }
+
+    return output;
+}
+
+pybind11::object convert_to_python(const millijson::Base* x) {
+    if (x->type() == millijson::NOTHING) {
+        return pybind11::none();
+    } else if (x->type() == millijson::BOOLEAN) {
+        return pybind11::bool_(reinterpret_cast<const millijson::Boolean*>(x)->value);
+    } else if (x->type() == millijson::NUMBER) {
+        return pybind11::float_(reinterpret_cast<const millijson::Number*>(x)->value);
+    } else if (x->type() == millijson::STRING) {
+        return pybind11::str(reinterpret_cast<const millijson::String*>(x)->value);
+    } else if (x->type() == millijson::ARRAY) {
+        const auto& y = reinterpret_cast<const millijson::Array*>(x)->values;
+        pybind11::list output(y.size());
+        for (size_t i = 0, end = y.size(); i < end; ++i) {
+            output[i] = convert_to_python(y[i].get());
+        }
+        return output;
+    } else if (x->type() == millijson::OBJECT) {
+        const auto& y = reinterpret_cast<const millijson::Object*>(x)->values;
+        pybind11::dict output;
+        for (const auto& pair : y) {
+            output[pair.first.c_str()] = convert_to_python(pair.second.get());
+        }
+        return output;
+    } else {
+        throw std::runtime_error("unknown millijson type '" + std::to_string(x->type()) + "'");
+        return pybind11::none();
+    }
+}
+
+pybind11::dict convert_to_python(const takane::ObjectMetadata& x) {
+    pybind11::dict output;
+    output["type"] = x.type;
+    for (const auto& pair : x.other) {
+        output[pair.first.c_str()] = convert_to_python(pair.second.get());
+    }
+    return output;
+}
+
+void validate(std::string path, pybind11::handle metadata) {
+    if (pybind11::isinstance<pybind11::none>(metadata)) {
+        takane::validate(path, global_options);
+    } else {
+        auto converted = convert_to_millijson(metadata);
+        auto objmeta = takane::reformat_object_metadata(converted.get());
+        takane::validate(path, objmeta, global_options);
+    }
+}
+
+template<class Registry>
+bool has_existing(const std::string& type, const Registry& registry, const std::string& existing) {
+    auto it = registry.find(type);
+    if (it == registry.end()) {
+        return false;
+    }
+    if (existing == "error") {
+        throw std::runtime_error("function has already been registered for object type '" + type + "'");
+    }
+    return (existing == "old");
+}
+
+template<class Registry>
+bool deregister(std::string type, Registry& registry) {
+    auto it = registry.find(type);
+    if (it != registry.end()) {
+        registry.erase(it);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void register_validate_function(std::string type, pybind11::function fun, std::string existing) {
+    if (!has_existing(type, global_options.custom_validate, existing)) {
+        global_options.custom_validate[type] = [fun](const std::filesystem::path& path, const takane::ObjectMetadata& metadata, takane::Options&) {
+            fun(pybind11::str(path.c_str()), convert_to_python(metadata));
+            return;
+        };
+    }
+}
+
+bool deregister_validate_function(std::string type) {
+    return deregister(type, global_options.custom_validate);
 }
--- src/dolomite_base/validate_object.py
+++ src/dolomite_base/validate_object.py
@@ -1,16 +1,45 @@
 from . import lib_dolomite_base as lib
 
 
-def validate_object(path: str):
-    """Validate an on-disk representation of an object,
-    using validators based on the **takane** specifications.
+def validate_object(path: str, metadata: Optional[Dict] = None):
+    """
+    Validate an on-disk representation of an object, typically using validators
+    based on the **takane** specifications. Applications may also register their
+    own validators via :py:func:`~register_validate_object_function`. 
 
     Args:
         path: 
             Path to the directory containing the object's representation.
+
+        metadata:
+            Metadata for the object. If None, this is read from the ``OBJECT``
+            file in the ``path``.
 
     Raise:
         Error if the validation fails.
     """
     lib.validate(path)
     return
+
+
+def register_validate_object_function(type: str, fun: Optional[Callable] = None): 
+    """
+    Register a validator function for use by :py:func:`~validate_object`.
+
+    Args:
+        type:
+            Object type, as specified in the ``type`` property of the
+            ``OBJECT`` file.
+
+        fun:
+            Function that accepts a path to a directory (string) and
+            JSON-derived metadata (dictionary) and raises an error if the
+            object in the directory is not valid for the specified object type.
+
+            Alternatively None, in which case any previously registered
+            validator for ``type`` is removed.
+    """
+    if fun is None:
+        lib.deregister_validate_object_function(type)
+    else:
+        lib.register_validate_object_function(type, fun)
