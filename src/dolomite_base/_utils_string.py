26e9c442c60a66dd8a1fb2cfa23ac633e3c3d787
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2024-02-08 06:42:16 -0800

    Optimize loading of string vectors from H5 files (#23).

--- src/dolomite_base/_utils_string.py
+++ src/dolomite_base/_utils_string.py
@@ -1,5 +1,6 @@
 from typing import List
 import h5py
+import numpy
 
 
 def save_fixed_length_strings(handle: h5py.Group, name: str, x: List[str]) -> h5py.Dataset:
@@ -19,7 +20,7 @@ def save_fixed_length_strings(handle: h5py.Group, name: str, x: List[str]) -> h5
         ``x`` is saved into the group as a fixed-length string dataset,
         and a NumPy dataset handle is returned.
     """
-    tmp = [ y.encode("UTF8") for y in x ]
+    tmp = [ y.encode("UTF-8") for y in x ]
     maxed = 1
     for b in tmp:
         if len(b) > maxed:
@@ -28,15 +29,19 @@ def save_fixed_length_strings(handle: h5py.Group, name: str, x: List[str]) -> h5
 
 
 def load_string_vector_from_hdf5(handle: h5py.Dataset) -> List[str]:
-    output = list(handle)
-    if len(output) and isinstance(output[0], bytes):
-        for i, x in enumerate(output):
-            output[i] = x.decode("UTF-8")
+    output = handle[:]
+
+    if len(output):
+        _output = []
+        for x in output:
+            _out = x.decode("UTF-8") if isinstance(x, (bytes, numpy.bytes_)) else x
+            _output.append(_out)
+        output = _output
     return output
 
 
 def load_scalar_string_attribute_from_hdf5(handle, name: str) -> str:
     output = handle.attrs[name]
-    if isinstance(output, bytes):
+    if isinstance(output, (bytes, numpy.bytes_)):
         output = output.decode("UTF-8")
     return output
